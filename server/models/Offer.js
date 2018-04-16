bf1a35dee968d9ed30d528df779d045b2ea2d1f0
Ben Sooraj <bensoorajm@gmail.com>
2018-04-16 23:54:01 +0530

    Updated Offer model, to accommodate GeoJSON

--- server/models/Offer.js
+++ server/models/Offer.js
@@ -2,42 +2,49 @@ const mongoose = require('mongoose');
 
 var offerSchema = new mongoose.Schema({
     offerID: {
-        type: String,
+        $type: String,
         default: shortid.generate
     },
     user: {
-        type: mongoose.Schema.Types.ObjectId,
+        $type: mongoose.Schema.Types.ObjectId,
         ref: 'User',
         required: [true, "An offer must be associated with one user or establishment"]
     },
-    pickUpLoc: {
-        address: { type: String },
-        lat: { type: Number },
-        long: { type: Number }
+    location: {
+        type: {
+            $type: String,
+            default:'Point',
+            required: [true, "GeoJSON requires a type"]
+        },
+        address: {$type: String},
+        coordinates: {
+            $type: [Number],
+            required: [true, "GeoJSON requires latitude and longitude coordinates"]
+        }
     },
     description: {
-        type: String,
+        $type: String,
         default: 'No description available'
     },
     photos: [{
-        type: String,
+        $type: String,
     }],
     status: {
-        type: String,
-        enum: ['active', 'inactive'],
-        default: 'active'
+        $type: String,
+        enum: ['new', 'inProgress', 'closed'],
+        default: 'new'
     },
     items: [{
-        name: { type: String },
-        serving: { type: Number },
-        veg: { type: Boolean }
+        name: { $type: String },
+        serving: { $type: Number },
+        veg: { $type: Boolean }
     }],
     foodHub: {
-        type: mongoose.Schema.Types.ObjectId,
+        $type: mongoose.Schema.Types.ObjectId,
         ref: 'FoodHub',
     }
 
-}, { timestamps: true });
+}, { timestamps: true, typeKey: '$type' });
 
 var Offer = mongoose.model('Offer', offerSchema);
 
