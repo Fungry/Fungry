8d561b0cb1e641091d640f42f77407d90ae6c9ad
Ben Sooraj <bensoorajm@gmail.com>
2018-04-19 12:53:08 +0530

    added a function to remove a client when a ws connection is dropped.

--- server/webSockets/Clients.js
+++ server/webSockets/Clients.js
@@ -4,10 +4,15 @@ class Clients {
     constructor() {
         this.clientList = {};
         this.saveClient = this.saveClient.bind(this);
+        this.removeClient = this.removeClient.bind(this);
     }
 
     saveClient(username, client) {
         this.clientList[username] = client;
+    }
+
+    removeClient(username) {
+        delete this.clientList[username];
     }
 }
 
